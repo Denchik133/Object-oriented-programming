ff3843cb940e7318fa75d70c6cde993797a6bb7e
Denis <d.klimenko@istu.edu.ua>
2025-11-03 17:13:18 +0200

    Add files via upload

---
+++ tema11.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string encrypt(string original) {
+	string result = original;
+	for (char& c : result) {
+		c = c + 3;
+	}
+	return result;
+}
+
+string encrypt(string original, int shift) {
+	string result = original;
+	for (char& c : result) {
+		c = c + shift;
+	}
+	return result;
+}
+
+void encrypt(istream& in, ostream& out, int shift) {
+	char c;
+	while ((c = in.get()) != EOF) {
+		out.put(c + shift);
+	}
+}
+
+void encrypt(istream& in, ostream& out) {
+	char c;
+	while ((c = in.get()) != EOF) {
+		out.put(c + 3);
+	}
+}
+
+bool encryptFile(const string& inputFile, const string& outputFile, int shift) {
+	ifstream fin(inputFile);
+	if (!fin) {
+		cout << "Помилка роботи з вхідним файлом";
+		return false;
+	}
+	ofstream fout(outputFile);
+	if (!fout) {
+		cout << "Помилка роботи з вихідним файлом";
+		return false;
+	}
+	encrypt(fin, fout, shift);
+}
+
+bool encryptFile(const string& inputFile, const string& outputFile) {
+	ifstream fin(inputFile);
+	if (!fin) {
+		cout << "Помилка роботи з вхідним файлом";
+		return false;
+	}
+	ofstream fout(outputFile);
+	if (!fout) {
+		cout << "Помилка роботи з вихідним файлом";
+		return false;
+	}
+	encrypt(fin, fout);
+}
+
+int main() {
+	string inputFile = "input.txt";
+	string outputFile = "output.txt";
+	int shift;
+	cout << "Введіть зсув для шифрування: ";
+	cin >> shift;
+	if (shift <= 0) {
+		cout << "Помилка: зсув повинен бути додатним числом\n";
+		return 1;
+	}
+	else if (encryptFile(inputFile, outputFile, shift)) {
+		cout << "Файл успішно зашифровано зі зсувом " << shift << "\n";
+	}
+	else {
+		cout << "Помилка під час шифрування\n";
+	}
+}
