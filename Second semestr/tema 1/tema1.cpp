24c776f041a936e32d24b8f3a03a8f170241363c
Denis <d.klimenko@istu.edu.ua>
2026-02-07 22:17:18 +0200

    Add files via upload

--- Second semestr/tema 1/tema1.cpp
+++ Second semestr/tema 1/tema1.cpp
@@ -1,49 +1,76 @@
 #include <iostream>
 #include <list>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-void inputList(list<double>& list, int n) {
-    double x;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        list.push_back(x);
-    }
-}
-
-bool checkForMinusThree(const list<double>& list) {
-    for (auto it = list.begin(); it != list.end(); ++it) {
-        if (*it < -3) {
-            return true;
+class NumberEncryption {
+private:
+    list<double> data;
+    vector<char> encryptedData;
+public:
+    void inpupData(int n) {
+        double x;
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            data.push_back(x);
         }
     }
-    return false;
-}
 
-void listProcessing(list<double>& list, bool checkForMinusThree) {
-    if (checkForMinusThree) {
-        for (auto it = list.begin(); it != list.end(); ++it) {
-            if (*it < 0) {
-                *it = (*it) * (*it);
+    bool checkForMinusThree() const {
+        for (auto it = data.begin(); it != data.end(); ++it) {
+            if (*it < -3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void transform() {
+        if (checkForMinusThree()) {
+            for (auto it = data.begin(); it != data.end(); ++it) {
+                if (*it < 0) {
+                    *it = (*it) * (*it);
+                }
+            }
+        }
+        else {
+            for (auto it = data.begin(); it != data.end(); ++it) {
+                *it *= 0.1;
             }
         }
     }
-    else {
-        for (auto it = list.begin(); it != list.end(); ++it) {
-            *it *= 0.1;
+
+    void printReverse() const {
+        for (auto it = data.rbegin(); it != data.rend(); ++it) {
+            cout << *it << " ";
         }
     }
-}
-
-void listReverse(const list<double>& list) {
-    for (auto it = list.rbegin(); it != list.rend(); ++it) {
-        cout << *it << " ";
+    
+    void encrypt() {
+        encryptedData.clear();
+        for (auto it = data.begin(); it != data.end(); ++it) {
+            int integerPart = static_cast<int>(floor(*it));
+            if (integerPart % 2 == 0) {
+                encryptedData.push_back('E');
+            }
+            else {
+                encryptedData.push_back('O');
+            }
+        }
     }
-}
+
+    void printEncryptedData() const {
+        for (char c : encryptedData) {
+            cout << c << " ";
+        }
+    }
+};
 
 int main() {
     int n;
-    list<double> numbers;
+    NumberEncryption enc;
     cout << "Введіть кількість елементів списку: ";
     cin >> n;
     if (n <= 0) {
@@ -51,10 +78,12 @@ int main() {
         return 1;
     }
     cout << "Введіть елементи списку: ";
-    inputList(numbers, n);
-    bool condition = checkForMinusThree(numbers);
-    listProcessing(numbers, condition);
+    enc.inpupData(n);
+    enc.transform();
     cout << "Результат у зворотньому порядку: ";
-    listReverse(numbers);
+    enc.printReverse();
+    enc.encrypt();
+    cout << "\nЗашифровані дані: ";
+    enc.printEncryptedData();
     return 0;
 }
