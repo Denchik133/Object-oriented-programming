ad7daed58ae6e971a66522bc94aea18eb62e1942
Denis <fourtnight.dragon@gmail.com>
2026-02-21 09:19:05 +0200

    Add files via upload

---
+++ Tims/tims3/tims3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        try
+        {
+            double R;
+            cout << "Введіть радіус круга: ";
+            cin >> R;
+            bool validInput = !cin.fail() && R > 0;
+            if (!validInput) {
+                throw invalid_argument("Некоректний ввід або радіус <= 0.");
+            }
+            double circleArea = 3.14 * R * R;
+            double side = (2 * R) / sqrt(2);
+            double squareArea = side * side;
+            double probability = squareArea / circleArea;
+            cout << "Ймовірність попадання точки у квадрат = " << probability << "\n";
+            break;
+        }
+        catch (const exception& e)
+        {
+            cout << "\033[31mПомилка: " << e.what() << "\033[0m\n";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+    return 0;
+}
