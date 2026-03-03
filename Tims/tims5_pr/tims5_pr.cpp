c4629e375831c50c453ff9b708172d4c48b06440
Denis <fourtnight.dragon@gmail.com>
2026-03-03 12:38:00 +0200

    Add files via upload

---
+++ Tims/tims5_pr/tims5_pr.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        try {
+            double p;
+            int desiredMode;
+            cout << "Введіть ймовірність проростання (0 < p < 1): ";
+            cin >> p;
+            cout << "Введіть бажане найімовірніше число зерен, що НЕ зійшли: ";
+            cin >> desiredMode;
+            bool validInput = !cin.fail() && p > 0 && p < 1 && desiredMode >= 0;
+            if (!validInput) {
+                throw invalid_argument("Некоректні дані.");
+            }
+            double q = 1 - p;
+            double left = desiredMode / q;
+            double right = (desiredMode + 1) / q;
+            int n_min = ceil(left - 1);
+            int n_max = floor(right - 1);
+            if (n_min > n_max) {
+                throw runtime_error("Розв'язку не існує для введених даних.");
+            }
+            cout << "\nМожливі значення n від " << n_min << " до " << n_max << endl;
+            cout << "Найменше можливе n = " << n_min << endl;
+            break;
+        }
+        catch (const exception& e) {
+            cout << "\033[31mПомилка: " << e.what() << "\033[0m\n";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+    return 0;
+}
