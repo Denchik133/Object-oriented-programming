db9589e7e4c68cae73615ebedd1d7f2c20008595
Denis <fourtnight.dragon@gmail.com>
2026-03-06 10:58:01 +0200

    Add files via upload

--- Second semestr/lab04/lab04.cpp
+++ Second semestr/lab04/lab04.cpp
@@ -1,25 +1,60 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <locale>
 
 using namespace std;
 
 class CesarCipher {
 private:
     int shift;
+    int mode;
+
 public:
-    CesarCipher(int s) {
-        shift = s % 26;
+    CesarCipher(int s, int m) {
+        shift = s;
+        mode = m;
     }
-    string encrypt(const string& text) {
-        string result = text;
-        for (char& c : result) {
-            if (isupper(c)) {
-                c = 'A' + (c - 'A' + shift + 26) % 26;
+
+    wstring encrypt(const wstring& text)
+    {
+        const wstring latinUpper = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const wstring latinLower = L"abcdefghijklmnopqrstuvwxyz";
+
+        const wstring cyrUpper = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        const wstring cyrLower = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        const wstring digits = L"0123456789";
+        wstring result = text;
+        for (wchar_t& c : result) {
+            if (mode == 1) {
+                size_t pos = latinUpper.find(c);
+                if (pos != wstring::npos) {
+                    c = latinUpper[(pos + shift % 26 + 26) % 26];
+                    continue;
+                }
+                pos = latinLower.find(c);
+                if (pos != wstring::npos) {
+                    c = latinLower[(pos + shift % 26 + 26) % 26];
+                    continue;
+                }
             }
-            else if (islower(c)) {
-                c = 'a' + (c - 'a' + shift + 26) % 26;
+            if (mode == 2) {
+                size_t pos = cyrUpper.find(c);
+                if (pos != wstring::npos) {
+                    c = cyrUpper[(pos + shift % cyrUpper.size() + cyrUpper.size()) % cyrUpper.size()];
+                    continue;
+                }
+                pos = cyrLower.find(c);
+                if (pos != wstring::npos) {
+                    c = cyrLower[(pos + shift % cyrLower.size() + cyrLower.size()) % cyrLower.size()];
+                    continue;
+                }
+            }
+            size_t pos = digits.find(c);
+            if (pos != wstring::npos) {
+                c = digits[(pos + shift % 10 + 10) % 10];
             }
         }
         return result;
@@ -30,10 +65,12 @@ template<typename T>
 class Stack {
 private:
     vector<T> data;
+
 public:
     void push(const T& value) {
         data.push_back(value);
     }
+
     T pop() {
         if (data.empty()) {
             throw runtime_error("Стек порожній!");
@@ -42,6 +79,7 @@ public:
         data.pop_back();
         return value;
     }
+
     T longest() const {
         if (data.empty()) {
             throw runtime_error("Стек порожній!");
@@ -54,16 +92,19 @@ public:
         }
         return maxStr;
     }
+
     void print() const {
         for (const auto& s : data) {
-            cout << s << endl;
+            wcout << s << endl;
         }
     }
 };
 
-int main()
-{
-    Stack<string> cipherStack;
+int main() {
+    setlocale(LC_ALL, "");
+    locale::global(locale(""));
+    wcout.imbue(locale());
+    Stack<wstring> cipherStack;
     int shift;
     while (true) {
         cout << "Введіть зсув для шифру Цезаря: ";
@@ -75,7 +116,21 @@ int main()
         cin.clear();
         cin.ignore(10000, '\n');
     }
-    CesarCipher cipher(shift);
+    int mode;
+    while (true) {
+        cout << "\nОберіть алфавіт:\n";
+        cout << "1 - Латиниця\n";
+        cout << "2 - Кирилиця\n";
+        cout << "Ваш вибір: ";
+        cin >> mode;
+        if (!cin.fail() && (mode == 1 || mode == 2)) {
+            break;
+        }
+        cout << "\033[31mНекоректний вибір.\033[0m\n";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    CesarCipher cipher(shift, mode);
     int n;
     while (true) {
         cout << "Скільки повідомлень додати у стек: ";
@@ -88,22 +143,22 @@ int main()
         cin.ignore(10000, '\n');
     }
     cin.ignore();
-    cout << "Введіть повідомлення:\n";
+    wcout << L"Введіть повідомлення:\n";
     for (int i = 0; i < n; i++) {
-        string text;
+        wstring text;
         while (true) {
-            getline(cin, text);
+            getline(wcin, text);
             if (!text.empty()) {
                 break;
             }
-            cout << "\033[31mРядок не може бути порожнім. Введіть ще раз:\033[0m\n";
+            wcout << L"\033[31mРядок не може бути порожнім. Введіть ще раз:\033[0m\n";
         }
-        string encrypted = cipher.encrypt(text);
+        wstring encrypted = cipher.encrypt(text);
         cipherStack.push(encrypted);
     }
-    cout << "\nЗашифровані повідомлення у стеку:\n";
+    wcout << L"\nЗашифровані повідомлення у стеку:\n";
     cipherStack.print();
-    cout << "\nНайдовший зашифрований рядок:\n";
-    cout << cipherStack.longest() << endl;
+    wcout << L"\nНайдовший зашифрований рядок:\n";
+    wcout << cipherStack.longest() << endl;
     return 0;
 }
